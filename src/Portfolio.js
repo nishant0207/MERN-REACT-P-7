bbf3772a64c04b245908c9a4c7ca62032d36d459
nishant0207 <dalalnishant0207@gmail.com>
2023-10-21 15:57:23 +0530

    first commit

---
+++ src/Portfolio.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const Portfolio = () => {
+  const [msg, setMsg] = useState('');
+
+  // Your component logic and JSX content here
+
+  return (
+    <div>
+      {/* Your JSX content goes here */}
+    </div>
+  );
+};
+
+export default Portfolio;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Portfolio from './Portfolio'; // Import your Portfolio component
+
+function App() {
+  return (
+    <div className="App">
+      {/* Your logo and other content */}
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
